872aae4ab9fcbb5058dfad2072d5b6899cde68b8
kapil-valecha <kapil.valecha@systematixindia.com>
2018-09-07 19:52:43 +0530

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Institute.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //[CustomAuthorize (Roles ="Admin,Teacher,Student")] //all users
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+          public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        }
+}
